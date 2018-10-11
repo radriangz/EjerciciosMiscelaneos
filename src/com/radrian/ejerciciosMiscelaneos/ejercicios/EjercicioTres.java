f726ff405ebc6e3b78890c43ddad0831ed28ca97
radriangz <radriangz@outlook.com>
2018-10-11 12:28:16 -0500

    Se creó la clase EjercicioTres, para el tercer ejercicio: Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida por consola. También se comenzó a programar el código en MainClass para llamar los métodos de los ejercicios.

---
+++ src/com/radrian/ejerciciosMiscelaneos/ejercicios/EjercicioTres.java
@@ -0,0 +1,17 @@
+package com.radrian.ejerciciosMiscelaneos.ejercicios;
+
+/**
+ * Ejercicio 3) Declara un String que contenga tu nombre, después muestra un 
+ * mensaje de bienvenida por consola. Por ejemplo: si introduzco “Fernando”, 
+ * me aparezca “Bienvenido Fernando”.
+ * 
+ * @author RAdrian
+ */
+public class EjercicioTres {
+	public void metodoEjercicioTres(String nombreUsuario) {
+		if (nombreUsuario != null) {
+			System.out.println("Bienvenido " + nombreUsuario);
+		}
+		
+	}
+}
--- src/com/radrian/ejerciciosMiscelaneos/main/MainClass.java
+++ src/com/radrian/ejerciciosMiscelaneos/main/MainClass.java
@@ -1,5 +1,9 @@
 package com.radrian.ejerciciosMiscelaneos.main;
 
+import com.radrian.ejerciciosMiscelaneos.ejercicios.EjercicioDos;
+import com.radrian.ejerciciosMiscelaneos.ejercicios.EjercicioTres;
+import com.radrian.ejerciciosMiscelaneos.ejercicios.EjercicioUno;
+
 /**
  * Esta clase contiene los llamados a las clases de los ejercicios.
  * 
@@ -10,12 +14,28 @@ public class MainClass {
 
 	/**
 	 * Método Principal
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		
-		
+
+		byte inputUsuario = 0;
+		float valorA = 1.5f;
+		float valorB = 2.5f;
+		String nombreUsuario = "Adrian";
+
+		if (inputUsuario == 1) {
+			EjercicioUno ejercicio1 = new EjercicioUno();
+			ejercicio1.metodoEjercicio(valorA, valorB);
+
+		} else if (inputUsuario == 2) {
+			EjercicioDos ejercicio2 = new EjercicioDos();
+			ejercicio2.metodoEjercicioDos(valorA, valorB);
+
+		} else if (inputUsuario == 3) {
+			EjercicioTres ejercicio3 = new EjercicioTres();
+			ejercicio3.metodoEjercicioTres(nombreUsuario);
+		}
 	}
 
 }
