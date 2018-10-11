0711703cfe1a62c585708ec98246493ca266b583
radriangz <radriangz@outlook.com>
2018-10-11 07:47:57 -0500

    Primer Commit. Se creó clase main en el proyecto, se configuró el .gitignore

---
+++ src/com/radrian/ejerciciosMiscelaneos/main/MainClass.java
@@ -0,0 +1,21 @@
+package com.radrian.ejerciciosMiscelaneos.main;
+
+/**
+ * Esta clase contiene los llamados a las clases de los ejercicios.
+ * 
+ * @author RAdrian
+ *
+ */
+public class MainClass {
+
+	/**
+	 * Método Principal
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
