4087ae490f87a0075bf7077bef02cefbcc4acc61
radriangz <radriangz@outlook.com>
2018-10-29 15:00:53 -0600

    Se creó la interfaz Entregable, con los métodos entregar(), devolver(), isEntregado(), y compareTo(Object a).

---
+++ src/com/radrian/ejerciciosPOO/ejercicio05/Entregable.java
@@ -0,0 +1,16 @@
+package com.radrian.ejerciciosPOO.ejercicio05;
+
+/**
+ * @author RAdrian
+ *
+ */
+public interface Entregable {
+	public void entregar();
+	
+	public void devolver();
+	
+	public boolean isEntregado();
+	
+	public void compareTo(Object a);
+	
+}
